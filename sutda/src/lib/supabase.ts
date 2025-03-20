a4ff250ddfd39559320c8c7e0242dbc719de8e25
SC516 <my_subb01@proton.me>
2025-03-20 18:07:21 +0900

    실시간

--- sutda/src/lib/supabase.ts
+++ sutda/src/lib/supabase.ts
@@ -1,8 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/types/supabase';
 
-// 환경 변수 설정이 필요합니다
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey); 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  realtime: {
+    params: {
+      eventsPerSecond: 10
+    }
+  }
+});
